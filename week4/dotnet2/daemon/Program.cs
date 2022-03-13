ea5eba09d28acce172ee7bdd2ebf8bec19398a49
Hung-Wei Chiu <hungwei@opennetworking.org>
2022-03-13 16:18:18 -0700

    Upload manifest for week4

---
+++ week4/dotnet2/daemon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+
+
+namespace daemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
